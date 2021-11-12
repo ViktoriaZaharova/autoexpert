f2a82dd7649502b5a02ec156ca40dbb065aeefd7
Viktoria <zaharova181114@gmail.com>
2021-11-12 21:52:59 +0200

    page1

--- app/js/common.js
+++ app/js/common.js
@@ -3,6 +3,14 @@ $('.attention-slider').slick({
     fade: true,
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+    responsive: [
+        {
+            breakpoint: 576,
+            settings: {
+                arrows: false
+            }
+        }
+    ]
 });
 
 
@@ -12,4 +20,20 @@ $('.btn-burger').on('click', function () {
 
 $('.mobile-menu__close').on('click', function () {
     $('.mobile-menu').fadeOut();
+});
+
+$('.td-mobile-toggle').on('click', function () {
+    var
+        $this = $(this),
+        content = $(this).siblings('td');
+
+    if(!$this.hasClass('trigger')){
+        $this.addClass('trigger');
+
+        content.css('display', 'flex');
+    } else {
+        $this.removeClass('trigger');
+
+        content.css('display', 'none');
+    }
 });
