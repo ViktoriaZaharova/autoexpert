23fbfff9400eb7eeb1c77094237d78e5911f7d1f
Viktoria <zaharova181114@gmail.com>
2021-11-12 10:28:30 +0200

    table

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('.attention-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>',
+});
+
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.mobile-menu__close').on('click', function () {
+    $('.mobile-menu').fadeOut();
 });
